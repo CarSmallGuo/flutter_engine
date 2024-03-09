9c277144791909a30a6e88efddcb3e711cf89a1f
Rulong Chen（陈汝龙） <rulongchen@live.com>
2024-03-09 08:50:47 +0000

    update shell/platform/ohos/ohos_xcomponent_adapter.h.
    Signed-off-by: Rulong Chen（陈汝龙） <rulongchen@live.com>

--- shell/platform/ohos/ohos_xcomponent_adapter.h
+++ shell/platform/ohos/ohos_xcomponent_adapter.h
@@ -28,59 +28,59 @@ namespace flutter {
 
 class XComponentBase {
  public:
-  explicit XComponentBase(const std::string& id);
-  ~XComponentBase();
+    explicit XComponentBase(const std::string& id);
+    ~XComponentBase();
 
-  void AttachFlutterEngine(const std::string& shell_holder_id);
-  void DetachFlutterEngine();
-  void SetNativeXComponent(OH_NativeXComponent* native_xcomponent);
+    void AttachFlutterEngine(const std::string& shell_holder_id);
+    void DetachFlutterEngine();
+    void SetNativeXComponent(OH_NativeXComponent* native_xcomponent);
 
-  // Callback, called by ACE XComponent
-  void OnSurfaceCreated(OH_NativeXComponent* component, void* window);
-  void OnSurfaceChanged(OH_NativeXComponent* component, void* window);
-  void OnSurfaceDestroyed(OH_NativeXComponent* component, void* window);
-  void OnDispatchTouchEvent(OH_NativeXComponent* component, void* window);
+    // Callback, called by ACE XComponent
+    void OnSurfaceCreated(OH_NativeXComponent* component, void* window);
+    void OnSurfaceChanged(OH_NativeXComponent* component, void* window);
+    void OnSurfaceDestroyed(OH_NativeXComponent* component, void* window);
+    void OnDispatchTouchEvent(OH_NativeXComponent* component, void* window);
 
-  OH_NativeXComponent* native_xcomponent() const { return native_xcomponent_; }
+    OH_NativeXComponent* native_xcomponent() const { return native_xcomponent_; }
 
  private:
-  void BindXComponentCallback();
+    void BindXComponentCallback();
 
-  OH_NativeXComponent_TouchEvent touch_event_;
-  OH_NativeXComponent_Callback callback_;
-  std::string id_;
-  std::string shell_holder_id_;
-  bool isEngineAttached_;
-  OH_NativeXComponent* native_xcomponent_;
-  void* window_;
-  uint64_t width_;
-  uint64_t height_;
-  OhosTouchProcessor ohos_touch_processor_;
+    OH_NativeXComponent_TouchEvent touch_event_;
+    OH_NativeXComponent_Callback callback_;
+    std::string id_;
+    std::string shell_holder_id_;
+    bool isEngineAttached_;
+    OH_NativeXComponent* native_xcomponent_;
+    void* window_;
+    uint64_t width_;
+    uint64_t height_;
+    OhosTouchProcessor ohos_touch_processor_;
 };
 
 using XComponentBaseMap = std::map<std::string, XComponentBase*>;
 class XComponentAdapter {
  public:
-  static XComponentAdapter* GetInstance();
+    static XComponentAdapter* GetInstance();
 
-  // Forbid copy and assign
-  XComponentAdapter(const XComponentAdapter&) = delete;
-  XComponentAdapter& operator=(const XComponentAdapter&) = delete;
+    // Forbid copy and assign
+    XComponentAdapter(const XComponentAdapter&) = delete;
+    XComponentAdapter& operator=(const XComponentAdapter&) = delete;
 
-  bool Export(napi_env env, napi_value exports);
-  void SetNativeXComponent(const std::string& id,
-                           OH_NativeXComponent* native_xcomponent);
-  void AttachFlutterEngine(const std::string& id,
-                           const std::string& shell_holder_id);
-  void DetachFlutterEngine(const std::string& id);
+    bool Export(napi_env env, napi_value exports);
+    void SetNativeXComponent(const std::string& id,
+                             OH_NativeXComponent* native_xcomponent);
+    void AttachFlutterEngine(const std::string& id,
+                             const std::string& shell_holder_id);
+    void DetachFlutterEngine(const std::string& id);
 
-  XComponentBaseMap& xcomponent_map() { return xcomponent_map_; }
+    XComponentBaseMap& xcomponent_map() { return xcomponent_map_; }
 
  private:
-  XComponentAdapter();
-  ~XComponentAdapter();
+    XComponentAdapter();
+    ~XComponentAdapter();
 
-  XComponentBaseMap xcomponent_map_;
+    XComponentBaseMap xcomponent_map_;
 };
 
 }  // namespace flutter
