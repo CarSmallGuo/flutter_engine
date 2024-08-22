e21b2c9f540d807394b6289a6e4028c223958d12
hyiso <mylaoda@qq.com>
2024-08-22 16:11:48 +0800

    增加 setTextureBufferSize napi 接口，允许外部设置 Texture 的 Buffer 大小
    Signed-off-by: hyiso <mylaoda@qq.com>

--- shell/platform/ohos/napi/platform_view_ohos_napi.h
+++ shell/platform/ohos/napi/platform_view_ohos_napi.h
@@ -179,6 +179,10 @@ class PlatformViewOHOSNapi {
       napi_env env,
       napi_callback_info info);
 
+  static napi_value nativeSetTextureBufferSize(
+    napi_env env,
+    napi_callback_info info);
+
   // Surface相关，XComponent调用
   static void SurfaceCreated(int64_t shell_holder, void* window);
 
