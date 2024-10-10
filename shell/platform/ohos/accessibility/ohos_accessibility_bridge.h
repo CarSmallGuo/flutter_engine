733cf8ac7fc766f366f3a94702f8b11eb452e88d
zjxi <xizijun1@huawei.com>
2024-10-10 09:55:37 +0800

    fix:删除无用成员字段
    Signed-off-by: zjxi <xizijun1@huawei.com>

--- shell/platform/ohos/accessibility/ohos_accessibility_bridge.h
+++ shell/platform/ohos/accessibility/ohos_accessibility_bridge.h
@@ -143,7 +143,6 @@ class OhosAccessibilityBridge {
   static const int32_t ROOT_NODE_ID = 0;
   constexpr static const double SCROLL_EXTENT_FOR_INFINITY = 100000.0;
   constexpr static const double SCROLL_POSITION_CAP_FOR_INFINITY = 70000.0;
-  int32_t accessibilityFeatureFlags = 0;
 
   flutter::SemanticsNode inputFocusedNode;
   flutter::SemanticsNode lastInputFocusedNode;
