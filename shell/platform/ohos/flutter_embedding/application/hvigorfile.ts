b3950953e28ec086a6515aeb9eed8d5a53c0e04a
chenzhensheng79 <15118029047@139.com>
2025-02-05 17:21:40 +0800

    更新版权信息
    Signed-off-by: chenzhensheng79 <15118029047@139.com>

--- shell/platform/ohos/flutter_embedding/application/hvigorfile.ts
+++ shell/platform/ohos/flutter_embedding/application/hvigorfile.ts
@@ -1,8 +1,4 @@
-/*
- * Copyright (c) 2021-2024 Huawei Device Co., Ltd. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE_HW file.
- */
+
 import { hapTasks } from '@ohos/hvigor-ohos-plugin';
 
 export default {
