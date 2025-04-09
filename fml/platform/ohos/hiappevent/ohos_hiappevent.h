7d51721512ad8f983e675afa02e34e360976e8c2
lwx0221 <741744990@qq.com>
2025-04-09 14:50:54 +0800

    新增性能探测雷达丢帧上报功能
    Signed-off-by: lwx0221 <741744990@qq.com>

---
+++ fml/platform/ohos/hiappevent/ohos_hiappevent.h
@@ -0,0 +1,96 @@
+#ifndef OHOS_HIAPPEVENT_H
+#define OHOS_HIAPPEVENT_H
+
+#include <vector>
+#include <hiappevent/hiappevent.h>
+
+namespace fml{
+
+namespace hiappevent{
+
+using CreateProcessorFunc = HiAppEvent_Processor*(*)(const char* name);
+using SetReportRouteFunc = int (*)(HiAppEvent_Processor* processor, 
+                                   const char* appId,
+                                   const char* routeInfo);
+using SetReportPoliceFunc = int (*)(HiAppEvent_Processor* processor,
+                                   int periodReport, int batchReport, 
+                                   bool onStartReport, bool onBackgroundReport);
+using SetReportEventFunc = int(*)(HiAppEvent_Processor* processor,
+                                  const char* domain, const char* name,
+                                  bool isRealTime);
+using AddFunc = int64_t (*)(HiAppEvent_Processor* processor);
+using AddStringParam = ParamList (*)(ParamList list,
+                                    const char* name,
+                                    const char* str);
+using AddInt32Param = ParamList (*)(ParamList list,
+                                   const char* name,
+                                   int32_t num);
+using AddInt64Param = ParamList (*)(ParamList list,
+                                    const char* name,
+                                    int64_t num);
+using Write = int (*)(const char* domain,
+                      const char* name,
+                      enum EventType type,
+                      const ParamList list);
+using DestroyParamList = void (*)(ParamList list);
+using DestroyProcessor = void (*)(HiAppEvent_Processor* processor);
+
+typedef struct  MissedFrameInfo{
+    int64_t timestamp_micros;
+    int64_t time_tmp;
+    int64_t lastest_time_tmp;
+    int vsync_transitions_missed;
+} MissedFrameInfo;
+
+
+class OhosHiappEventDDL {
+public:
+    OhosHiappEventDDL(void);
+    ~OhosHiappEventDDL();
+
+    static constexpr char HiAppEvent_LIB_NAME[] = "libhiappevent_ndk.z.so";
+
+    void init(void);
+
+    static OhosHiappEventDDL* GetInstance(void);
+
+    void reportMissedFrameEvent(int64_t timestamp_micros,
+                                const char** argument_values,
+                                intptr_t argument_count);
+
+    void flush(void);
+
+    void apiGet(void);
+
+private:
+    void* libhiappevent_ndk_handler_ = nullptr;
+    CreateProcessorFunc createProcessorFunc_ = nullptr;
+    SetReportRouteFunc setReportRouteFunc_ = nullptr;
+    SetReportPoliceFunc setReportPoliceFunc_ = nullptr;
+    SetReportEventFunc setReportEventFunc_ = nullptr;
+    AddFunc addFunc_ = nullptr;
+    AddStringParam addStringParam_ = nullptr;
+    AddInt32Param addInt32Param_ = nullptr;
+    AddInt64Param addInt64Param_ = nullptr;
+    Write write_ = nullptr;
+    DestroyParamList destroyParamList_ = nullptr;
+    DestroyProcessor destroyProcessor_ = nullptr;
+
+
+    int apiVrsion_ = 0;
+
+    static OhosHiappEventDDL* instance_;
+
+    bool isValid_ = false;
+
+    bool isInit_ = false;
+
+    std::vector<MissedFrameInfo> MissedFrameInfos;
+
+};
+
+};
+};
+
+
+#endif
