d389efe656af71bd8da0cabacb5bcf643154dd59
liujiake <liujiake999@163.com>
2025-04-14 21:15:38 +0800

    处理内存泄露
    Signed-off-by: liujiake <liujiake999@163.com>

--- shell/platform/ohos/ohos_shell_holder.cpp
+++ shell/platform/ohos/ohos_shell_holder.cpp
@@ -629,5 +629,8 @@ int32_t OHOSShellHolder::GetAccessibilityNodeCursorPosition(int64_t elementId,
   return bridge_->GetAccessibilityNodeCursorPosition(elementId, index);
 }
 
+std::shared_ptr<PlatformViewOHOSNapi> OHOSShellHolder::GetNapiFacade() {
+  return napi_facade_;
+}
 
 }  // namespace flutter
