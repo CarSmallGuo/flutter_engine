384a9fb650d6908d73ebc42893a4722732cf5076
MTChannn <MTChannn@gmail.com>
2025-04-25 16:08:56 +0800

    添加动态加载库工具类
    Signed-off-by: MTChannn <MTChannn@gmail.com>

--- shell/platform/ohos/ohos_touch_processor.h
+++ shell/platform/ohos/ohos_touch_processor.h
@@ -64,18 +64,19 @@ public:
     ~OhosTouchProcessor();
  
 private:
-   int apiVersion_;
-   // 共享库名称
-   static constexpr char UIInputEvent_LIB_NAME[] = "libace_ndk.z.so";
-   // 类型别名，用于表示动态加载的函数指针类型
-   using GetDeviceIdFunc = int32_t (*)(ArkUI_UIInputEvent*);
-   using GetAxisActionFunc = int32_t (*)(ArkUI_UIInputEvent*);
-   using GetModifierKeyStatesFunc = int32_t (*)(ArkUI_UIInputEvent*, uint64_t*);
-   // 成员变量：保存动态加载的函数指针和库句柄
-   void* localLibHandler_;
-   GetDeviceIdFunc dynamicGetDeviceId_;
-   GetAxisActionFunc dynamicGetAxisAction_;
-   GetModifierKeyStatesFunc dynamicGetModifierKeyStates_;
+    float accumulatedDeltaX_ = 0.0;
+    float accumulatedDeltaY_ = 0.0;
+    float accumulatedScale_ = 1.0;
+
+private:
+    int apiVersion_;
+    std::unique_ptr<DynamicLibraryLoader> loader_;
+    // 共享库名称
+    static constexpr char UI_INPUT_EVENT_LIB_NAME[] = "libace_ndk.z.so";
+    // 动态加载的函数指针
+    int32_t (*dynamicGetDeviceId_)(ArkUI_UIInputEvent*);
+    int32_t (*dynamicGetAxisAction_)(ArkUI_UIInputEvent*);
+    int32_t (*dynamicGetModifierKeyStates_)(ArkUI_UIInputEvent*, uint64_t*);
 
 private:
     std::shared_ptr<std::string[]> packagePacketData(std::unique_ptr<OhosTouchProcessor::TouchPacket> touchPacket);
