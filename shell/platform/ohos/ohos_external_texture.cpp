385cb1679644184b247612e4e5dd6f34ceb25f41
MTChannn <MTChannn@gmail.com>
2025-04-29 15:00:29 +0800

    修正外接纹理第一帧背景色颜色格式为ABGR
    Signed-off-by: MTChannn <MTChannn@gmail.com>

--- shell/platform/ohos/ohos_external_texture.cpp
+++ shell/platform/ohos/ohos_external_texture.cpp
@@ -377,9 +377,16 @@ bool OHOSExternalTexture::SetPixelMapAsProducer(
   return end_ret;
 }
 
-void OHOSExternalTexture::SetBackgroundColor(uint32_t color)
-{
-  backGroundColor_ = color;
+void OHOSExternalTexture::SetBackgroundColor(uint32_t color) {
+  // ABGR to ARGB
+  uint8_t a = (color >> 24) & 0xFF;
+  uint8_t b = (color >> 16) & 0xFF;
+  uint8_t g = (color >>  8) & 0xFF;
+  uint8_t r =  color        & 0xFF;
+  backGroundColor_ = (static_cast<uint32_t>(a) << 24) |
+                     (static_cast<uint32_t>(r) << 16) |
+                     (static_cast<uint32_t>(g) <<  8) |
+                     (static_cast<uint32_t>(b)      );
 }
 
 void OHOSExternalTexture::ReleaseWindowBuffer(OH_NativeImage* native_image,
