dgh1818/flutter_engine_ohos

Prevent deadlock when the external texture's producer task runs before OnNativeImageFrameAvailable task in platform thread.
Signed-off-by: Gao Di <gaodi.sec@qq.com>

engine/src/flutter/shell/platform/ohos/platform_view_ohos.cpp
@@ -37,7 +37,7 @@ namespace flutter {
 
 // This global map's key is (texture_id)
 std::map<uint64_t, PlatformViewOHOS*> g_texture_platformview_map;
-std::mutex g_map_mutex;
+std::recursive_mutex g_map_mutex;
 
 OhosSurfaceFactoryImpl::OhosSurfaceFactoryImpl(
     const std::shared_ptr<OHOSContext>& context)
@@ -159,7 +159,7 @@ void PlatformViewOHOS::NotifyCreate(
       // because the onGrContextCreate method of the external texture will be
       // called in PlatformView::NotifyCreated.
       RegisterTexture(external_texture);
-      std::lock_guard<std::mutex> lock(g_map_mutex);
+      std::lock_guard<std::recursive_mutex> lock(g_map_mutex);
       g_texture_platformview_map[(uint64_t)texture_id] = this;
     }
 
@@ -200,7 +200,7 @@ void PlatformViewOHOS::Preload(int width, int height) {
 
     for (auto [texture_id, external_texture] : all_external_texture_) {
       RegisterTexture(external_texture);
-      std::lock_guard<std::mutex> lock(g_map_mutex);
+      std::lock_guard<std::recursive_mutex> lock(g_map_mutex);
       g_texture_platformview_map[(uint64_t)texture_id] = this;
     }
 
@@ -303,7 +303,7 @@ void PlatformViewOHOS::NotifyDestroyed() {
       // unregisterExternalTexture, their actual destruction will occur after
       // ~PlatformViewOHOS.
       UnregisterTexture(texture_id);
-      std::lock_guard<std::mutex> lock(g_map_mutex);
+      std::lock_guard<std::recursive_mutex> lock(g_map_mutex);
       g_texture_platformview_map.erase((uint64_t)texture_id);
     }
     fml::AutoResetWaitableEvent latch;
@@ -574,7 +574,7 @@ std::shared_ptr<OHOSExternalTexture> PlatformViewOHOS::CreateExternalTexture(
   }
   if (extrenal_texture && extrenal_texture->GetProducerSurfaceId() != 0 &&
       extrenal_texture->GetProducerWindowId() != 0) {
-    std::lock_guard<std::mutex> lock(g_map_mutex);
+    std::lock_guard<std::recursive_mutex> lock(g_map_mutex);
     g_texture_platformview_map[context_frame_data] = this;
     all_external_texture_[texture_id] = extrenal_texture;
     RegisterTexture(extrenal_texture);
@@ -602,7 +602,7 @@ uint64_t PlatformViewOHOS::GetExternalTextureWindowId(int64_t texture_id) {
 
 void PlatformViewOHOS::OnNativeImageFrameAvailable(void* data) {
   uint64_t ptexture_id = (uint64_t)data;
-  std::lock_guard<std::mutex> lock(g_map_mutex);
+  std::lock_guard<std::recursive_mutex> lock(g_map_mutex);
   if (g_texture_platformview_map.find(ptexture_id) ==
       g_texture_platformview_map.end()) {
     return;
@@ -615,17 +615,21 @@ void PlatformViewOHOS::OnNativeImageFrameAvailable(void* data) {
     return;
   }
 
-  // Note: RunNowOrPostTask may get dead lock when running in platform thread.
-  platform->task_runners_.GetPlatformTaskRunner()->PostTask([ptexture_id]() {
-    std::lock_guard<std::mutex> lock(g_map_mutex);
-    if (g_texture_platformview_map.find(ptexture_id) ==
-        g_texture_platformview_map.end()) {
-      return;
-    }
-    PlatformViewOHOS* platform = g_texture_platformview_map[ptexture_id];
-    uint64_t texture_id = ptexture_id;
-    platform->MarkTextureFrameAvailable(texture_id);
-  });
+  // Note: PostTask may lead to a deadlock if a render task (which might acquire
+  // the buffer) is dispatched earlier and scheduled to run before this task.
+  // So we use recursive_mutex to safely invoke OnNativeImageFrameAvailable from
+  // the platform thread, allowing nested lock acquisition without deadlock.
+  fml::TaskRunner::RunNowOrPostTask(
+      platform->task_runners_.GetPlatformTaskRunner(), [ptexture_id]() {
+        std::lock_guard<std::recursive_mutex> lock(g_map_mutex);
+        if (g_texture_platformview_map.find(ptexture_id) ==
+            g_texture_platformview_map.end()) {
+          return;
+        }
+        PlatformViewOHOS* platform = g_texture_platformview_map[ptexture_id];
+        uint64_t texture_id = ptexture_id;
+        platform->MarkTextureFrameAvailable(texture_id);
+      });
 }
 
 void PlatformViewOHOS::UnRegisterExternalTexture(int64_t texture_id) {
@@ -641,7 +645,7 @@ void PlatformViewOHOS::UnRegisterExternalTexture(int64_t texture_id) {
                                     [&latch]() { latch.Signal(); });
   latch.Wait();
 
-  std::lock_guard<std::mutex> lock(g_map_mutex);
+  std::lock_guard<std::recursive_mutex> lock(g_map_mutex);
   g_texture_platformview_map.erase((uint64_t)texture_id);
 }
 
@@ -666,14 +670,14 @@ void PlatformViewOHOS::SetExternalTextureBackGroundPixelMap(
   }
 }
 
-void PlatformViewOHOS::SetExternalTextureBackGroundColor(
-    int64_t texture_id,
-    uint32_t color) {
-    if (all_external_texture_.find(texture_id) != all_external_texture_.end()) {
-      auto external_texture = all_external_texture_[texture_id];
-      FML_LOG(INFO) << "SetExternalTextureBackGroundColor " << texture_id << " color " << color;
-      external_texture->SetBackGroundColor(color);
-    }
+void PlatformViewOHOS::SetExternalTextureBackGroundColor(int64_t texture_id,
+                                                         uint32_t color) {
+  if (all_external_texture_.find(texture_id) != all_external_texture_.end()) {
+    auto external_texture = all_external_texture_[texture_id];
+    FML_LOG(INFO) << "SetExternalTextureBackGroundColor " << texture_id
+                  << " color " << color;
+    external_texture->SetBackGroundColor(color);
+  }
 }
 
 void PlatformViewOHOS::SetTextureBufferSize(int64_t texture_id,

CarSmallGuo/flutter_engine

shell/platform/ohos/platform_view_ohos.cpp
@@ -23,7 +23,7 @@ namespace flutter {
 // This global map's key is (PlatformViewOHOS-ptr + texture_id) because there
 // may be many platformViews.
 std::map<uint64_t, PlatformViewOHOS*> g_texture_platformview_map;
-std::mutex g_map_mutex;
+std::recursive_mutex g_map_mutex;
 
 OhosSurfaceFactoryImpl::OhosSurfaceFactoryImpl(
     const std::shared_ptr<OHOSContext>& context,
@@ -132,7 +132,7 @@ void PlatformViewOHOS::NotifyCreate(
       // because the onGrContextCreate method of the external texture will be
       // called in PlatformView::NotifyCreated.
       RegisterTexture(external_texture);
-      std::lock_guard<std::mutex> lock(g_map_mutex);
+      std::lock_guard<std::recursive_mutex> lock(g_map_mutex);
       g_texture_platformview_map[(uint64_t)texture_id] = this;
     }
 
@@ -217,7 +217,7 @@ void PlatformViewOHOS::NotifyDestroyed() {
       // unregisterExternalTexture, their actual destruction will occur after
       // ~PlatformViewOHOS.
       UnregisterTexture(texture_id);
-      std::lock_guard<std::mutex> lock(g_map_mutex);
+      std::lock_guard<std::recursive_mutex> lock(g_map_mutex);
       g_texture_platformview_map.erase((uint64_t)texture_id);
     }
     fml::AutoResetWaitableEvent latch;
@@ -467,7 +467,7 @@ PointerDataDispatcherMaker PlatformViewOHOS::GetDispatcherMaker() {
 
 void PlatformViewOHOS::OnNativeImageFrameAvailable(void* data) {
   uint64_t ptexture_id = (uint64_t)data;
-  std::lock_guard<std::mutex> lock(g_map_mutex);
+  std::lock_guard<std::recursive_mutex> lock(g_map_mutex);
   if (g_texture_platformview_map.find(ptexture_id) ==
       g_texture_platformview_map.end()) {
     return;
@@ -480,17 +480,21 @@ void PlatformViewOHOS::OnNativeImageFrameAvailable(void* data) {
     return;
   }
 
-  // Note: RunNowOrPostTask may get dead lock when running in platform thread.
-  platform->task_runners_.GetPlatformTaskRunner()->PostTask([ptexture_id]() {
-    std::lock_guard<std::mutex> lock(g_map_mutex);
-    if (g_texture_platformview_map.find(ptexture_id) ==
-        g_texture_platformview_map.end()) {
-      return;
-    }
-    PlatformViewOHOS* platform = g_texture_platformview_map[ptexture_id];
-    uint64_t texture_id = ptexture_id - (uint64_t)platform;
-    platform->MarkTextureFrameAvailable(texture_id);
-  });
+  // Note: PostTask may lead to a deadlock if a render task (which might acquire
+  // the buffer) is dispatched earlier and scheduled to run before this task.
+  // So we use recursive_mutex to safely invoke OnNativeImageFrameAvailable from
+  // the platform thread, allowing nested lock acquisition without deadlock.
+  fml::TaskRunner::RunNowOrPostTask(
+      platform->task_runners_.GetPlatformTaskRunner(), [ptexture_id]() {
+        std::lock_guard<std::recursive_mutex> lock(g_map_mutex);
+        if (g_texture_platformview_map.find(ptexture_id) ==
+            g_texture_platformview_map.end()) {
+          return;
+        }
+        PlatformViewOHOS* platform = g_texture_platformview_map[ptexture_id];
+        uint64_t texture_id = ptexture_id - (uint64_t)platform;
+        platform->MarkTextureFrameAvailable(texture_id);
+      });
 }
 
 std::shared_ptr<OHOSExternalTexture> PlatformViewOHOS::CreateExternalTexture(
@@ -509,7 +513,7 @@ std::shared_ptr<OHOSExternalTexture> PlatformViewOHOS::CreateExternalTexture(
   }
   if (extrenal_texture && extrenal_texture->GetProducerSurfaceId() != 0 &&
       extrenal_texture->GetProducerWindowId() != 0) {
-    std::lock_guard<std::mutex> lock(g_map_mutex);
+    std::lock_guard<std::recursive_mutex> lock(g_map_mutex);
     g_texture_platformview_map[context_frame_data] = this;
     external_texture_gl_[texture_id] = extrenal_texture;
     RegisterTexture(extrenal_texture);
@@ -564,7 +568,7 @@ void PlatformViewOHOS::UnRegisterExternalTexture(int64_t texture_id) {
                                     [&latch]() { latch.Signal(); });
   latch.Wait();
 
-  std::lock_guard<std::mutex> lock(g_map_mutex);
+  std::lock_guard<std::recursive_mutex> lock(g_map_mutex);
   g_texture_platformview_map.erase((uint64_t)this + (uint64_t)texture_id);
 }
 
@@ -596,17 +600,13 @@ void PlatformViewOHOS::SetExternalTextureBackGroundPixelMap(
   }
 }
 
-void PlatformViewOHOS::SetExternalTextureBackGroundColor(
-    int64_t texture_id,
-    uint32_t color) {
-  if (ohos_context_->RenderingApi() == OHOSRenderingAPI::kOpenGLES) {
-    auto iter = external_texture_gl_.find(texture_id);
-    if (iter != external_texture_gl_.end()) {
-      auto external_texture = external_texture_gl_[texture_id];
-      FML_LOG(INFO) << "SetExternalTextureBackGroundColor " << texture_id
-                    << "color:0x" << std::hex << color;
-      external_texture->SetBackgroundColor(color);
-    }
+void PlatformViewOHOS::SetExternalTextureBackGroundColor(int64_t texture_id,
+                                                         uint32_t color) {
+  if (external_texture_gl_.find(texture_id) != external_texture_gl_.end()) {
+    auto external_texture = external_texture_gl_[texture_id];
+    FML_LOG(INFO) << "SetExternalTextureBackGroundColor " << texture_id
+                  << " color " << color;
+    external_texture->SetBackgroundColor(color);
   }
 }
 