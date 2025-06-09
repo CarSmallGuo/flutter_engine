304a0771f9615be1cd37e1071d4161618fb1c945
MTChannn <MTChannn@gmail.com>
2025-06-09 11:57:38 +0800

    重构轴事件处理，适配更多场景
    Signed-off-by: MTChannn <MTChannn@gmail.com>

--- shell/platform/ohos/ohos_xcomponent_adapter.cpp
+++ shell/platform/ohos/ohos_xcomponent_adapter.cpp
@@ -241,6 +241,18 @@ void DispatchTouchEventCB(OH_NativeXComponent* component, void* window) {
   }
 }
 
+void DispatchAxisEventCB(OH_NativeXComponent* component,
+                         ArkUI_UIInputEvent* event,
+                         ArkUI_UIInputEvent_Type type) {
+  std::lock_guard<std::mutex> lock(
+      XComponentAdapter::GetInstance()->xcomponentMap_mutex_);
+  for (auto it : XComponentAdapter::GetInstance()->xcomponetMap_) {
+    if (it.second->nativeXComponent_ == component) {
+      it.second->OnDispatchAxisEvent(component, event, type);
+    }
+  }
+}
+
 void DispatchMouseEventCB(OH_NativeXComponent* component, void* window) {
   std::lock_guard<std::mutex> lock(
       XComponentAdapter::GetInstance()->xcomponentMap_mutex_);
@@ -494,6 +506,8 @@ void XComponentBase::SetNativeXComponent(
     OH_NativeXComponent_RegisterCallback(nativeXComponent_, &callback_);
     OH_NativeXComponent_RegisterMouseEventCallback(nativeXComponent_,
                                                    &mouseCallback_);
+    OH_NativeXComponent_RegisterUIInputEventCallback(
+        nativeXComponent_, DispatchAxisEventCB, ARKUI_UIINPUTEVENT_TYPE_AXIS);
   }
 }
 
@@ -676,6 +690,20 @@ void XComponentBase::OnDispatchTouchEvent(OH_NativeXComponent* component,
       LOGE(
           "XComponentManger::DispatchTouchEvent XComponentBase is not "
           "attached");
+    }
+  }
+}
+
+void XComponentBase::OnDispatchAxisEvent(OH_NativeXComponent* component,
+                                         ArkUI_UIInputEvent* event,
+                                         ArkUI_UIInputEvent_Type type) {
+  if (type == ARKUI_UIINPUTEVENT_TYPE_AXIS) {
+    if (isEngineAttached_) {
+      ohosTouchProcessor_.HandleAxisEvent(std::stoll(shellholderId_), component,
+                                          event);
+    } else {
+      LOGE(
+          "XComponentManger::DispatchAxisEvent XComponentBase is not attached");
     }
   }
 }
